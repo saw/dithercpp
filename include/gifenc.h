bea8b312cea41d431ec82a5dbb4b49153edc3281
Stephen Woods <saw@stephenwoods.net>
2026-02-15 11:05:25 -0800

    include source ref

--- include/gifenc.h
+++ include/gifenc.h
@@ -1,3 +1,5 @@
+// Source: https://github.com/lecram/gifenc
+
 #ifndef GIFENC_H
 #define GIFENC_H
 
--- source/gifenc.c
+++ source/gifenc.c
@@ -1,3 +1,5 @@
+// Source: https://github.com/lecram/gifenc
+
 #include "gifenc.h"
 
 #include <stdio.h>
